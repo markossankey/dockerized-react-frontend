6b66cac00f7d04672c0ea1713b3643fbbf9ac264
Markos Sankey <markossankey@icloud.com>
2023-02-17 20:57:05 -0600

    initializing template

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="flex align-middle justify-center flex-col text-center h-screen">
+      <h1 className="text-2xl underline">Vite React Template</h1>
+      <p>axios</p>
+      <p>@tanstack/react-query</p>
+      <p>zod</p>
+      <p>tailwind</p>
+    </div>
+  );
+}
+
+export default App;
